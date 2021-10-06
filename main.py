0e8d87eeafeeaf961d05349050a48a8f1525f2fb
Kesav Manoj <kesavmanoj@gmail.com>
2021-10-06 18:20:08 +0530

    added new file

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 from functions import *
 import sqlite3
+from main_functions import *
 
 
 root = Tk()
 root.title('Program') 
-root.geometry("500x500")
+root.geometry("449x400")
 
 
 # Connect to Database and create a cursor
 conn = sqlite3.connect('iscon.db')
 c = conn.cursor()
-
 
 # Create top buttons
 employees_btn = Button(root, text = "Employees", width = 20, height = 2, command = employees_btn).grid(row = 0, column = 0)
@@ -21,11 +21,19 @@ designation_btn = Button(root, text = 'Designations', width = 20, height = 2, co
 project_btn = Button(root, text = 'Projects', width = 20, height = 2, command = projects_btn).grid(row = 0, column = 2)
 
 space_inBetween = Label(root, text = "       ", width = 8, height = 2).grid(row = 1, column = 1)
-enter_employee_id_label = Label(root, text = "Enter Employee Number : ", width = 20, height = 2).grid(row = 2, column = 0)
+enter_project_number_label = Label(root, text = "Enter Project Number : ", width = 20, height = 2).grid(row = 2, column = 0)
+enter_project_number = Entry(root, width = 20)
+enter_project_number.grid(row = 2, column = 1)
+
+enter_employee_id_label = Label(root, text = "Enter Employee ID : ",  width = 20, height = 2).grid(row = 3, column = 0)
 enter_employee_id = Entry(root, width = 20)
-enter_employee_id.grid(row = 2, column = 1)
-enter_btn = Button(root, text = "Enter", width = 20, height = 1).grid(row = 2, column = 2)
- 
+enter_employee_id.grid(row = 3, column = 1)
+
+enter_hours_label = Label(root, text = 'No. of Hours Worked : ',  width = 20, height = 2).grid(row = 4, column = 0)
+enter_hours = Entry(root, width = 20)
+enter_hours.grid(row = 4, column = 1)
+
+enter_btn = Button(root, text = "Enter", width = 20, height = 2, command= find).grid(row = 5, column = 1)
 
 # Commit changes
 conn.commit()  
